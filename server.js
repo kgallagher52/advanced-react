111b185971851f9059f6bd60a1cf0728efc0b789
keaton <keatonrgallagher@gmail.com>
2024-08-23 21:37:07 -0600

    feat(created-basic): server

---
+++ server.js
@@ -0,0 +1,47 @@
+import { authors, books } from "./data";
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+let currentUser = {
+  name: "Sarah Waters",
+  age: 55,
+  country: "United Kingdom",
+  books: ["Fingersmith", "The Night Watch"],
+};
+
+let users = authors;
+
+app.get("/current-user", (req, res) => res.json(currentUser));
+
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  res.json(users.find((user) => user.id === id));
+});
+
+app.get("/users", (req, res) => res.json(users));
+
+app.post("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { user: editedUser } = req.body;
+
+  users = users.map((user) => (user.id === id ? editedUser : user));
+
+  res.json(users.find((user) => user.id === id));
+});
+
+app.get("/books", (req, res) => res.json(books));
+
+app.get("/books/:id", (req, res) => {
+  const { id } = req.params;
+  res.json(books.find((book) => book.id === id));
+});
+
+let SERVER_PORT = 9090;
+app.listen(SERVER_PORT, () =>
+  console.log(`Server is listening on port: ${SERVER_PORT}`)
+);
