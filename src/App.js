900dc78322e413365ec7150c21e0ced08a6a6278
keaton <keatonrgallagher@gmail.com>
2024-08-23 19:54:45 -0600

    feat(split-screen): basic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { SplitScreen } from "./components/split-screen";
 
-const LeftSideComp = () => (
-  <h2 style={{ background: "teal" }}>I am Left</h2>
+const LeftSideComp = ({ title }) => (
+  <h2 style={{ background: "teal" }}>{title}</h2>
 );
 
-const RightSideComp = () => (
-  <h2 style={{ background: "green" }}>I am Right</h2>
+const RightSideComp = ({ title }) => (
+  <h2 style={{ background: "green" }}>{title}</h2>
 );
 
 function App() {
   return (
     <SplitScreen leftWidth={1} rightWidth={3} >
-      <LeftSideComp />
-      <RightSideComp />
+      <LeftSideComp title={"I am Left!"} />
+      <RightSideComp title={"I am Right!"} />
     </SplitScreen>
   );
 };
