9b3bc1074a73dd0a9b7f9ffb7b2705ac657899d1
keaton <keatonrgallagher@gmail.com>
2024-09-02 21:59:17 -0600

    feat(uncontrolled-form): example

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
-import axios from "axios";
-import { BookInfo } from "./section2-3-layout-and-container-components/info/book";
-import { DataSourceRenderPattern } from "./section2-3-layout-and-container-components/data-source-with-render-pattern";
+import { UncontrolledForm } from "./section4";
 
-const getData = async (url) => {
-  const res = await axios.get(url);
-  return res.data;
-};
+// const getData = async (url) => {
+//   const res = await axios.get(url);
+//   return res.data;
+// };
+
 
 /* Double arrow function creates a higher order function */
-const getDataFromLocalStorage = (key) => () => {
-  const localData = localStorage.getItem(key);
-  return localData ? localData : `No data found with key: ${key}`;
-};
+// const getDataFromLocalStorage = (key) => () => {
+//   const localData = localStorage.getItem(key);
+//   return localData ? localData : `No data found with key: ${key}`;
+// };
 
 /* simple functional component to display local storage data */
-const Message = ({ msg }) => <h1>{msg}</h1>;
+// const Message = ({ msg }) => <h1>{msg}</h1>;
 
 function App() {
   return (
     <>
-      <DataSource
-        getData={() => getData('/books/1')}
-        resourceName={'book'}>
-        <BookInfo />
-      </DataSource>
-      <DataSource
-        getData={() => getDataFromLocalStorage('test')}
-        resourceName={'msg'}>
-        <Message />
-      </DataSource>
+      <UncontrolledForm />
     </>
   );
 };
